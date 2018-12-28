359a06ad6d5a158e4b6e5a77266f49c7a032f174
sanketwalunjkar <sanketwalunjkar1996@gmail.com>
2018-12-28 19:31:59 +0530

    add ajax call response

--- ajax/ajax.js
+++ ajax/ajax.js
@@ -1,5 +1,17 @@
-const xhttp = new XMLHttpRequest(), 
+const xhttp = new XMLHttpRequest(),
   log = console.log;
 xhttp.open('GET', 'https://jsonplaceholder.typicode.com/todos/', true);
-xhttp.onreadystatechange = () => xhttp.readyState === 4 && xhttp.status == 200 ? log(xhttp.responseText) : log(xhttp.readyState)
+xhttp.onreadystatechange = () => {
+  if (xhttp.readyState === 4 && xhttp.status == 200) {
+    const response = JSON.parse(xhttp.responseText),
+      selectElement = document.getElementById('demo');
+    response.map(item => {
+      const liElement = document.createElement('li');
+      liElement.innerHTML = item.title;
+      selectElement.appendChild(liElement);
+    });
+  } else {
+    log(xhttp.readyState)
+  }
+}
 xhttp.send();
